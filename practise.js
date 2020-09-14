41fcd8576c9ee837e81ef176bfa927a1b93a5e7d
ISIAKA ABDULAHI <isiakaabd@gmail.com>
2020-09-14 21:54:53 +0100

    new assignment added, a calculator that can perform basic arithmetic operation

---
+++ practise.js
@@ -0,0 +1,69 @@
+// to ensure a number is entered when it prompts
+do {
+    inputNumber1 = parseInt(prompt("Enter the first value"));
+    inputNumber2 = parseInt(prompt("Enter the second value value"));
+    // entering a number
+}
+while (!inputNumber1 || !inputNumber2)
+
+
+
+// function to use calculator
+//function expression
+
+function calculator(number1, number2) {
+    // to ensure one of the four options are selected and a number not on the list isn't imputed
+    do {
+        calcOperatioon = parseInt(prompt("Welcome to REMLAD SERVICE \n\n Pls select the operation you want to do \n 1. addition \n 2. subtraction \n 3. multiplication \n 4. Division "))
+    }
+    while (!calcOperatioon || calcOperatioon > 4)
+
+    // checking with the if-statement
+    var operation, display;
+    if (calcOperatioon === 1) {
+        operation = (number1 + number2);
+        display = `${number1} +  ${number2} `
+
+    } else if (calcOperatioon === 2) {
+        operation = number1 - number2;
+        display = `${number1} -  ${number2} `
+    } else if (calcOperatioon === 3) {
+        operation = number1 * number2
+        display = `${number1} ×  ${number2} `;
+
+    } else if (calcOperatioon === 4) {
+        operation = (number1 / number2);
+        display = `${number1} /  ${number2} `
+    }
+    return `${display} = ${operation} `;
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function calling
+alert(calculator(inputNumber1, inputNumber2))
